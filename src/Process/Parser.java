05ff2221708272603683031584bb107db9ed552f
marcos <marcosviniciussouzadossantos19@gmail.com>
2025-01-23 16:07:26 -0300

    adicionando parte da função

--- src/Process/Parser.java
+++ src/Process/Parser.java
@@ -5,6 +5,7 @@ import Model.SyntaxTree.Node;
 import Model.SyntaxTree.Tree;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Parser {
     static int openParentheses = 0;
@@ -26,7 +27,36 @@ public class Parser {
         return tree;
     }
 
-    public static Node GenerateElement(String value){
-        if
+    private int Precedence(String operator){
+        if ( operator.equals("+") || operator.equals("-")) return 1;
+        if ( operator.equals("*") || operator.equals("/")) return 2;
+        if ( operator.equals("^")) return 3;
+        return 0;
+    }
+
+    private void apply_operator(Stack<String> operators, Stack<Integer> values){
+        int right = values.pop();
+        int left  = values.pop();
+        String op = operators.pop();
+
+        if ( op.equals("+")){
+            values.add( left + right);
+        }
+
+        if ( op.equals("-")){
+            values.add( left - right);
+        }
+
+        if ( op.equals("*")){
+            values.add( left * right);
+        }
+
+        if ( op.equals("/")){
+            values.add( left / right);
+        }
+
+        if ( op.equals("^")){
+            values.add((int) Math.pow(left, right));
+        }
     }
 }
