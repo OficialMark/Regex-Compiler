224cc2c41b9964b57d9ebd4c74163f48eca0f3a3
marcos <marcosviniciussouzadossantos19@gmail.com>
2025-01-27 16:21:21 -0300

    terminando calculadora pura

--- src/Process/Parser.java
+++ src/Process/Parser.java
@@ -5,36 +5,73 @@ import Model.SyntaxTree.Node;
 import Model.SyntaxTree.Tree;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Parser {
     static int openParentheses = 0;
     static int closeParentheses = 0;
 
-    public static Tree GenerateTree(ArrayList<String> expressao){
-        Tree tree = new Tree();
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String regex = s.nextLine();
 
-        for (int i = 0; i < expressao.size(); i++){
-            String x = expressao.get(i);
-
-            if (x.equals("(")){ openParentheses++;}
-
-            if (x.equals(")")){ closeParentheses++;}
-        }
-
-
-
-        return tree;
+        int resultado = process_expression(regex);
     }
 
-    private int Precedence(String operator){
+    private static int process_expression(String expression){
+        Stack<String> operators = new Stack<>();
+        Stack<Integer> values   = new Stack<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+
+            if( expression.charAt(i) == ' ') continue;
+
+            if (Character.isDigit(expression.charAt(i))){
+                int value = 0;
+                while ( i < expression.length() && Character.isDigit(expression.charAt(i))){ //enquanto a cadeia não acabar e o próximo valor for um digito
+                    value = value * 10 + Integer.parseInt(String.valueOf(expression.charAt(i)));
+                    i++;
+                }
+                values.add(value);
+                continue;
+            }
+
+            if (expression.charAt(i) == '('){
+                operators.add(String.valueOf(expression.charAt(i)));
+            }
+
+            else if (expression.charAt(i) == ')'){
+                while (!operators.isEmpty() && operators.get(1).equals(")")){
+                    apply_operator(operators, values);
+                operators.pop();
+                }
+            }
+
+            else {
+                while (!operators.isEmpty() &&
+                        precedence(operators.get(1)) >= precedence(String.valueOf(expression.charAt(i)))){
+                            apply_operator(operators, values);
+                        }
+                operators.add(String.valueOf(expression.charAt(i)));
+            }
+        }
+
+        while (!operators.isEmpty()){
+            apply_operator(operators, values);
+        }
+
+        return values.getFirst();
+    }
+
+    private static int precedence(String operator){
         if ( operator.equals("+") || operator.equals("-")) return 1;
         if ( operator.equals("*") || operator.equals("/")) return 2;
         if ( operator.equals("^")) return 3;
         return 0;
     }
 
-    private void apply_operator(Stack<String> operators, Stack<Integer> values){
+    private static void apply_operator(Stack<String> operators, Stack<Integer> values){
         int right = values.pop();
         int left  = values.pop();
         String op = operators.pop();
